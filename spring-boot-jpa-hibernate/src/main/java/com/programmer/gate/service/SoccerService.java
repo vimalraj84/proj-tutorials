13766fd3aa04162a9811071e7646eca42490c836
Karunakaran <vkarunakaran@deloitte.com>
2019-05-05 10:27:22 -0400

    Changes to POM

--- spring-boot-jpa-hibernate/src/main/java/com/programmer/gate/service/SoccerService.java
+++
@@ -1,8 +0,0 @@
-package com.programmer.gate.service;
-
-import java.util.List;
-
-public interface SoccerService {
-	public List<String> getAllTeamPlayers(long teamId);
-	public void addBarcelonaPlayer(String name, String position, int number);
-}
