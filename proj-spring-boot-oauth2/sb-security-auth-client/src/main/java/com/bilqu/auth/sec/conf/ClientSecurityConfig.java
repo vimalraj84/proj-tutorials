a5493258157b2f7cbfa3c87396e03bc1c87804f5
Karunakaran <vkarunakaran@deloitte.com>
2019-07-23 08:53:31 -0400

    desktop update

--- proj-spring-boot-oauth2/sb-security-auth-client/src/main/java/com/bilqu/auth/sec/conf/ClientSecurityConfig.java
+++ proj-spring-boot-oauth2/sb-security-auth-client/src/main/java/com/bilqu/auth/sec/conf/ClientSecurityConfig.java
@@ -1,4 +1,6 @@
 package com.bilqu.auth.sec.conf;
 
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 public class ClientSecurityConfig extends WebSecurityConfigurerAdapter {
 }
